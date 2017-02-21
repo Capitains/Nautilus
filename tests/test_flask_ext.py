eab49bb01f3775a17bbb00c03c9724d2a8b4e74f
Thibault Clérice <leponteineptique@gmail.com>
2017-02-21 12:33:03 +0100

    Reducing log size

--- tests/test_flask_ext.py
+++ tests/test_flask_ext.py
@@ -36,7 +36,7 @@ class TestRestAPI(TestCase):
         self.parent = CTS("/cts")
         self.resolver = HttpCTSResolver(endpoint=self.parent)
         logassert.setup(self, nauti.logger.name)
-        nauti.logger.disabled
+        nautilus.logger.disabled = True
 
         def call(this, parameters={}):
             """ Call an endpoint given the parameters
@@ -433,7 +433,7 @@ class TestRestAPICache(TestRestAPI):
         self.parent = CTS("/cts")
         self.resolver = HttpCTSResolver(endpoint=self.parent)
         logassert.setup(self, nautilus.logger.name)
-        nautilus.logger.disabled
+        nautilus.logger.disabled = True
 
         def call(this, parameters={}):
             """ Call an endpoint given the parameters
