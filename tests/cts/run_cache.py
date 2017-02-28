19291fa72c2fa96614c56d0137abd4c0ce6ff254
Thibault Clérice <leponteineptique@gmail.com>
2017-02-28 17:07:42 +0100

    Reproduction system for bug in issue #44 [Caching graph and instance graph differs]

---
+++ tests/cts/run_cache.py
@@ -0,0 +1,7 @@
+from capitains_nautilus.cts.resolver import NautilusCTSResolver
+from werkzeug.contrib.cache import FileSystemCache
+from tests.cts import subprocess_repository, subprocess_cache_dir
+
+cache = FileSystemCache(subprocess_cache_dir)
+resolver = NautilusCTSResolver(resource=subprocess_repository, cache=cache)
+resolver.parse()
