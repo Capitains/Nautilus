422fe2ab183a5922db255537b53aaef5aad6a684
Thibault Clérice <leponteineptique@gmail.com>
2017-03-06 11:59:58 +0100

    1.0.0b5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='capitains_nautilus',
-  version="1.0.0b4",
+  version="1.0.0b5",
   description='Resolver for Capitains Guidelines Repository',
   url='http://github.com/Capitains/nautilus',
   author='Thibault Clerice',
