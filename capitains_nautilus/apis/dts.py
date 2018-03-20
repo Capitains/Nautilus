f5cb758c47ff86f1a4847a20e2c311686ae65894
Thibault Clérice <leponteineptique@gmail.com>
2018-03-20 14:27:25 +0100

    DTS

--- capitains_nautilus/apis/dts.py
+++ capitains_nautilus/apis/dts.py
@@ -1,12 +1,31 @@
-from flask import jsonify, request
+from flask import Response, request, url_for
 
+from lxml import etree
 from rdflib.namespace import DC
-from MyCapytain.common.constants import get_graph
+from rdflib import Literal, URIRef
+from MyCapytain.common.constants import get_graph, \
+    RDF_NAMESPACES, \
+    Mimetypes, \
+    XPATH_NAMESPACES
 
 from collections import defaultdict
+import json
 
 from capitains_nautilus.apis.base import AdditionalAPIPrototype
 from capitains_nautilus.errors import NautilusError
+
+
+def jsonify(response):
+    return Response(json.dumps(response), headers={
+        "Content-Type": "application/ld+json"
+    })
+
+
+def _nav_direction(collection, direction):
+    if direction == "children":
+        return collection.members
+    if direction == "parent":
+        return [collection.parent]
 
 
 def _resource_type(collection):
@@ -16,6 +35,8 @@ def _resource_type(collection):
 
 
 def _json_ld_value(obj):
+    if isinstance(obj, URIRef):
+        return str(obj)
     if obj.language:
         return {"@lang": obj.language, "@value": str(obj)}
     return str(obj)
@@ -33,21 +54,62 @@ def _dc_dictionary(triples):
         if pred.startswith(DC):
             *_, key = get_graph().namespace_manager.compute_qname(pred)
             output[key].append(_json_ld_value(obj))
+    if output:
+        output = {"dts:dublincore": output}
     return output
+
+
+def _metadata(triples):
+    """ Builds a dc dictionary based on the triples
+
+    :param triples: [(pred, obj)]
+    :return: Dictionary
+    :rtype: dict
+    """
+    output = defaultdict(list)
+    for pred, obj in triples:
+        if not pred.startswith(DC) and not pred.startswith(RDF_NAMESPACES.DTS):
+            key = get_graph().namespace_manager.qname(pred)
+            output[key].append(_json_ld_value(obj))
+    if output:
+        output = {"dts:extensions": output}
+    return output
+
+
+def _base_description(collection):
+    j = {
+        "@id": collection.id,
+        "@type": _resource_type(collection),
+        "title": collection.get_label(),
+        "totalItems": collection.size,
+    }
+    if hasattr(collection, "get_description"):
+        j["description"] = collection.get_description()
+    if collection.readable:
+        j["dts:passage"] = url_for(".dts_document", id=collection.id, _external=True)
+        j["dts:references"] = url_for(".dts_navigation", id=collection.id, _external=True)
+    return j
 
 
 class DTSApi(AdditionalAPIPrototype):
     NAME = "DTS"
     ROUTES = [
-        ('/dts/collections', "r_dts_collection", ["GET", "OPTIONS"])
+        ('/dts/', "r_dts_main", ["GET", "OPTIONS"]),
+        ('/dts/collections', "r_dts_collection", ["GET", "OPTIONS"]),
+        ('/dts/document', "r_dts_document", ["GET", "OPTIONS"]),
+        ('/dts/navigation', "r_dts_navigation", ["GET", "OPTIONS"])
     ]
     Access_Control_Allow_Methods = {
-        "r_dts_collection": "OPTIONS, GET"
+        "r_dts_collection": "OPTIONS, GET",
+        "r_dts_main": "OPTIONS, GET",
+        "r_dts_document": "OPTIONS, GET",
+        "r_dts_navigation": "OPTIONS, GET"
     }
     CACHED = [
         #  DTS
         "r_dts_collection",
-        "dts_error"
+        "dts_error",
+        "r_dts_main"
     ]
 
     def dts_error(self, error_name, message=None):
@@ -67,13 +129,25 @@ class DTSApi(AdditionalAPIPrototype):
         j.status_code = 404
         return j
 
+    def r_dts_main(self):
+        return jsonify({
+          "@context": "dts/EntryPoint.jsonld",
+          "@id": "dts/",
+          "@type": "EntryPoint",
+
+          "collections": url_for(".dts_collection", _external=True),
+          "documents": "dts/documents",
+          "navigation": "dts/navigation"
+        })
+
     def r_dts_collection(self):
         """ DTS Collection Metadata reply for given objectId
 
-        :param objectId: Collection Identifier
         :return: JSON Format of DTS Collection
         """
         objectId = request.args.get("id", None)
+        direction = request.args.get("nav", "children")
+
         try:
             collection = self.resolver.getMetadata(objectId=objectId)
             triples = list(collection.graph.predicate_objects(
@@ -86,25 +160,86 @@ class DTSApi(AdditionalAPIPrototype):
                     "dts": "https://w3id.org/dts/api#",
                     "tei": "http://www.tei-c.org/ns/1.0",
                 },
-                "@id": collection.id,
-                "@type": _resource_type(collection),
-                "title": collection.get_label(),
-                "totalItems": collection.size,
                 "members": [
-                    {
-                        "@id": member.id,
-                        "@type": _resource_type(member),
-                        "title": member.get_label()
-                    }
-                    for member in collection.members
+                    _base_description(member)
+                    for member in _nav_direction(collection, direction)
                 ]
             }
-            dc = _dc_dictionary(triples)
-            if len(dc):
-                j["dts:dublincore"] = dc
+            j.update(_base_description(collection))
+            j.update(_dc_dictionary(triples))
+            j.update(_metadata(triples))
 
         except NautilusError as E:
             return self.dts_error(error_name=E.__class__.__name__, message=E.__doc__)
         j = jsonify(j)
         j.status_code = 200
         return j
+
+    def r_dts_navigation(self):
+        objectId = request.args.get("id", None)
+        passageId = request.args.get("passage", None)
+        start = request.args.get("start", None)
+        end = request.args.get("end", None)
+        level = request.args.get("level", 1)
+        if not objectId:
+            raise Exception()
+        if start and end:
+            # Currently hacked to work only with CTS Identifier
+            # See https://github.com/Capitains/MyCapytain/issues/161
+            references = self.resolver.getReffs(
+                textId=objectId,
+                subreference="-".join([start, end]),
+                level=level
+            )
+        else:
+            references = self.resolver.getReffs(
+                textId=objectId,
+                subreference=passageId,
+                level=level
+            )
+        return jsonify({
+            "@context": {
+                "passage": "https://w3id.org/dts/api#passage"
+            },
+            "@base": url_for(".dts_document", _external=True),
+            "@id": objectId,
+            "passage": references
+        })
+
+    def r_dts_document(self):
+        objectId = request.args.get("id", None)
+        passageId = request.args.get("passage", None)
+        start = request.args.get("start", None)
+        end = request.args.get("end", None)
+        if not objectId:
+            raise Exception()
+        if start and end:
+            # Currently hacked to work only with CTS Identifier
+            # See https://github.com/Capitains/MyCapytain/issues/161
+            passage = self.resolver.getTextualNode(
+                textId=objectId,
+                subreference="-".join([start, end])
+            )
+        else:
+            passage = self.resolver.getTextualNode(
+                textId=objectId,
+                subreference=passageId
+            )
+
+        if passageId:
+            inputXML = passage.export(Mimetypes.PYTHON.ETREE)
+            wrapper = etree.fromstring("<dts:fragment xmlns:dts='https://w3id.org/dts/api#' />")
+            for container in inputXML.xpath("//tei:text", namespaces=XPATH_NAMESPACES):
+                container.getparent().append(wrapper)
+                wrapper.insert(0, container)
+                break
+            outputXML = etree.tostring(inputXML, encoding=str)
+        else:
+            outputXML = passage.export(Mimetypes.XML.TEI)
+
+        return Response(
+            outputXML,
+            headers={
+                "Content-Type": "application/tei+xml"
+            }
+        )
