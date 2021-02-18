3661d49ce45df9a39bd6db644d0e6f1709ed832c
Thibault Clérice <leponteineptique@gmail.com>
2021-02-18 09:44:53 +0100

    2.0.0 - Release removing support for TripleStore and SqlAlchemy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='capitains_nautilus',
-  version="1.0.3",
+  version="2.0.0",
   description='Resolver for Capitains Guidelines Repository',
   url='http://github.com/Capitains/nautilus',
   author='Thibault Clerice',
