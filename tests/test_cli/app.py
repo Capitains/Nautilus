4ebdb2907b617b85edb9c8c0018cc315a41911e1
Thibault Clérice <leponteineptique@gmail.com>
2021-02-18 09:04:55 +0100

    Move to github action

--- tests/test_cli/app.py
+++ tests/test_cli/app.py
@@ -57,5 +57,5 @@ nautilus = FlaskNautilus(
 #http_cache.init_app(app)
 app.debug = True
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+#if __name__ == "__main__":
+#    app.run(debug=True, host='0.0.0.0')
