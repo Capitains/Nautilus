118da4ac846eb99ee13ca561a4863ca1adfdff01
Thibault Clérice <leponteineptique@gmail.com>
2023-03-30 09:20:14 +0200

    STUPIDO

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
+import os
 
+here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'requirements.txt')) as f:
     REQUIRED = f.read().splitlines()
 
